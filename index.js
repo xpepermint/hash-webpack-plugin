cd1634a8e75b7c7bd2c41b99e3824e78318d2cf5
xpeper <xpepermint@gmail.com>
2015-04-11 01:00:51 +0200

    hash writer

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-asd
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(options) {
+  return function() {
+
+    var outputPath = options.path || './';
+    var fileName = options.fileName || 'hash.txt';
+
+    this.plugin('done', function(stats) {
+      fs.writeFileSync(
+        path.join(outputPath, fileName),
+        stats.hash
+      );
+    });
+  };
+};
