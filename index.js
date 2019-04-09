291232484281338a2f4babf5b653c8e8375d70fa
Jason Seminara <jason.seminara@lab49.com>
2019-04-09 16:35:31 -0400

    adds ability to handle different file types

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 
+/**
+ * @func makeFileContents
+ * @param {string} fileName - the user-inputted filename
+ * @returns the file contents based on the filetype.
+ **/
+function makeFileContents(fileName, { hash }) {
+  let hashString;
+
+  switch(path.extname(fileName)) {
+    case '.json':
+      /* json content should have json obj in the file */
+      hashString = JSON.stringify({ webpackHash: hash });
+      break;
+
+    default:
+      hashString = hash;
+      break;
+  }
+
+  return hashString;
+}
+
 module.exports = function(options) {
   return function() {
 
@@ -12,9 +34,10 @@ module.exports = function(options) {
       if (err) return console.log('Error creating folder:', err);
 
       this.plugin('done', function(stats) {
+
         fs.writeFileSync(
           path.join(outputPath, fileName),
-          stats.hash
+          makeFileContents(fileName, stats),
         );
       });
     }.bind(this));
